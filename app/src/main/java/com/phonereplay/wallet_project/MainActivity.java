5b3a3e23b2d886b7fd99d089590cd7ff17a945ff
thiago <thiiagosantos300@gmail.com>
2024-10-27 18:40:33 -0300

    initial commit

--- app/src/main/java/com/phonereplay/wallet_project/MainActivity.java
+++ app/src/main/java/com/phonereplay/wallet_project/MainActivity.java
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.Service;
 
+import org.bitcoinj.core.Coin;
 import org.bitcoinj.core.NetworkParameters;
 import org.bitcoinj.kits.WalletAppKit;
 import org.bitcoinj.params.TestNet3Params;
@@ -25,18 +26,18 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView addressText;
     private TextView privateKeyText;
+    private TextView balanceText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Inicializando as views
         addressText = findViewById(R.id.addressText);
         privateKeyText = findViewById(R.id.privateKeyText);
+        balanceText = findViewById(R.id.balanceText);
         Button createWalletButton = findViewById(R.id.createWalletButton);
 
-        // Configurar ação do botão para criar ou carregar carteira
         createWalletButton.setOnClickListener(v -> {
             if (walletAppKit == null || !walletAppKit.isRunning()) {
                 new WalletInitTask().execute();
@@ -63,13 +64,11 @@ public class MainActivity extends AppCompatActivity {
             NetworkParameters params = TestNet3Params.get();
             File walletDir = new File(getFilesDir(), "test_wallet");
 
-            // Verificar se o WalletAppKit já foi inicializado
             if (walletAppKit != null && walletAppKit.isRunning()) {
                 Log.d(TAG, "Carteira já existe. Carregando carteira existente.");
                 return walletAppKit.wallet();
             }
 
-            // Excluir carteira anterior, caso exista
             if (walletDir.exists()) {
                 for (File file : walletDir.listFiles()) {
                     file.delete();
@@ -79,10 +78,8 @@ public class MainActivity extends AppCompatActivity {
 
             walletAppKit = new WalletAppKit(params, walletDir, "wallet");
 
-            // Permitir que o WalletAppKit inicie sem bloquear a interface
             walletAppKit.setBlockingStartup(false);
 
-            // Adicionar listener para monitorar o estado do WalletAppKit
             walletAppKit.addListener(new Service.Listener() {
                 @Override
                 public void running() {
@@ -95,7 +92,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }, MoreExecutors.directExecutor());
 
-            // Inicializar e sincronizar a carteira
             walletAppKit.startAsync();
             Log.d(TAG, "WalletAppKit startAsync() chamado.");
 
@@ -114,14 +110,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(Wallet wallet) {
             if (wallet != null) {
-                // Atualizar a interface com o endereço e chave privada
                 String address = wallet.currentReceiveAddress().toString();
                 String privateKey = wallet.currentReceiveKey().getPrivateKeyEncoded(wallet.getParams()).toString();
                 addressText.setText(address);
                 privateKeyText.setText(privateKey);
 
+                Coin balance = wallet.getBalance();
+                balanceText.setText("Saldo: " + balance.toFriendlyString());
+
                 Log.d(TAG, "Bitcoin Address: " + address);
                 Log.d(TAG, "Private Key (WIF): " + privateKey);
+                Log.d(TAG, "Saldo da Carteira: " + balance.toFriendlyString());
             } else {
                 Log.e(TAG, "Falha ao inicializar a carteira. Verifique os logs para mais informações.");
             }
