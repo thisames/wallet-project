ef4b5d964016e8856e16c11efdaf699406d3b55b
emanuelleGued <manugguedes1701@gmail.com>
2024-11-18 18:49:38 -0300

    add configuration button in graph

--- app/src/main/java/com/phonereplay/wallet_project/GraphBitcoin.java
+++ app/src/main/java/com/phonereplay/wallet_project/GraphBitcoin.java
@@ -1,9 +1,11 @@
 package com.phonereplay.wallet_project;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -41,6 +43,8 @@ public class GraphBitcoin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph_bitcoin);
+        Button button = findViewById(R.id.button);
+
 
         currentPriceText = findViewById(R.id.currentPriceText);
 
@@ -72,10 +76,16 @@ public class GraphBitcoin extends AppCompatActivity {
         }
 
         currentPriceText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toggleCurrency();
-            }
+                                                @Override
+                                                public void onClick(View v) {
+                                                    toggleCurrency();
+                                                }
+                                            }
+        );
+
+        button.setOnClickListener(v -> {
+            Intent intent = new Intent(GraphBitcoin.this, GraphConfig.class);
+            startActivity(intent);
         });
 
         handler.postDelayed(new Runnable() {
