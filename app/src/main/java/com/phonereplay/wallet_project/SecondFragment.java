922cd52851a53638ab2087730d45266b89410716
thiago <thiiagosantos300@gmail.com>
2024-12-12 12:33:49 -0300

    refactor, create WalletAbstraction and WalletAbstractionInterface to make code more responsibility

--- app/src/main/java/com/phonereplay/wallet_project/SecondFragment.java
+++ app/src/main/java/com/phonereplay/wallet_project/SecondFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+
 import com.phonereplay.wallet_project.databinding.FragmentSecondBinding;
 
 public class SecondFragment extends Fragment {
@@ -21,6 +23,7 @@ public class SecondFragment extends Fragment {
     return binding.getRoot();
   }
 
+  @Override
   public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
 
